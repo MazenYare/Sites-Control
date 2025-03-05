4022b081c636bb52287353e52bba0f807533ed0c
MazenYare <mazensalwesch@gmail.com>
2025-03-06 00:10:56 +0200

    Deleting User Repostiry

--- lib/Features/Authentication/Domain/repository/user_repo.dart
+++
@@ -1,9 +0,0 @@
-import 'package:sites_project/Features/Home/Domain/models/site.dart';
-
-abstract class UserRepository {
-  Future<List<Site>> getUser();
-  Future<void> updateUser();
-  Future<void> isLoggedIn();
-  Future<void> logIn();
-  Future<void> logout();
-}
